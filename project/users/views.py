46a64061b314c357abb596b14614c3fec8e01230
Marek <korzeniowskimarek.mk@gmail.com>
2025-11-07 05:47:20 +0100

    sockerio added.

--- project/users/views.py
+++ project/users/views.py
@@ -75,3 +75,8 @@ def task_status(task_id: str):
 @users_router.get("/form_ws/")
 def form_ws_example(request: Request):
     return templates.TemplateResponse("form_ws.html", {"request": request})
+
+
+@users_router.get("/form_socketio/")
+def form_socketio_example(request: Request):
+    return templates.TemplateResponse("form_socketio.html", {"request": request})
